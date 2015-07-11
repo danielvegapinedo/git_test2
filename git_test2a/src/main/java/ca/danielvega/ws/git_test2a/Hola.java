3908558f3fd14dd2647dfecb3bbcb7e158ea1ef0
Daniel Vega 2 <danielvegapinedo@gmail.com>
2015-07-10 22:13:25 -0400

    Primer cambio

---
+++ git_test2a/src/main/java/ca/danielvega/ws/git_test2a/Hola.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.danielvega.ws.git_test2a;
+
+/**
+ *
+ * @author daniel
+ */
+public class Hola {
+    public static void main(String[] arg){
+        System.out.println("Hola");
+    }
+}
